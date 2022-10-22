28badfe2755b3abfe47842692aab8d9af0398555
Frank Gao <frankg0485@gmail.com>
2022-10-22 18:34:54 -0400

    added push notifications that work with a date & time picker

--- App.js
+++ App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
 import { Audio } from 'expo-av';
-import { getSongURL } from './backend';
+import { getSongURL } from './backend/youtube';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { schedulePushNotification, registerForPushNotificationsAsync } from './backend/notifications';
+import * as Notifications from 'expo-notifications';
+
 //import { Sound } from 'expo-av/build/Audio';
 
 //allows for song to be played in the background (doesn't work lol)
@@ -16,11 +20,41 @@ import { getSongURL } from './backend';
   playThroughEarpieceAndroid: false
 });*/
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
 export default function App() {
   const [url, onChangeURL] = React.useState('');
   const song = React.useRef(new Audio.Sound());
   const [indicator, setIndicator] = React.useState('');
+  const [dateTime, setDateTime] =  React.useState(new Date());
 
+  const [expoPushToken, setExpoPushToken] = React.useState('');
+  const [notification, setNotification] = React.useState(false);
+  const notificationListener = React.useRef();
+  const responseListener = React.useRef();
+
+  React.useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
 
   //TODO: make sure youtube URL is valid
   async function playSong() {
@@ -45,10 +79,40 @@ export default function App() {
     await song.current.playAsync();
   }
 
+  async function scheduleNotification() {
+    console.log(dateTime.getMinutes());
+    console.log(new Date().getMinutes());
+    const delay = dateTime.getTime() - new Date().getTime();
+    console.log(delay / 1000);
+    if (delay > 1000) {
+      await schedulePushNotification(delay);
+    } else {
+      console.log("Invalid date");
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Chorus</Text>
+      <DateTimePicker style={styles.dateTimePicker} onChange={(event, date) => setDateTime(date)} mode="datetime" value={dateTime}/>
       <View style={{flex: 0.1}} />
+      <View
+      style={{
+        flex: 0.5,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+      }}>
+      <Text>Your expo push token: {expoPushToken}</Text>
+      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Title: {notification && notification.request.content.title} </Text>
+        <Text>Body: {notification && notification.request.content.body}</Text>
+        <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+      </View>
+      <Button
+        title="Press to schedule a notification"
+        onPress={scheduleNotification}
+      />
+      </View>
       <View style={styles.inputRow}>
         <TextInput
           style={styles.input}
@@ -63,7 +127,7 @@ export default function App() {
         color="#841584"
         accessibilityLabel="Learn more about this purple button"/>
       </View>
-      <View style={{flex: 0.1}} />
+      <View style={{flex: 0.05}} />
       <Text style={styles.indicator}>
         {indicator}
       </Text>
@@ -96,5 +160,10 @@ const styles = StyleSheet.create({
   indicator: {
     fontSize: 20,
     letterSpacing: 4,
+  },
+
+  dateTimePicker: {
+    flexBasis: 'auto',
+    width: '50%',
   }
 });
